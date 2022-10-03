158a2d686a70d21581faf5e3f1292bd1d52803c7
MinsuKim <minsu020302@gmail.com>
2022-10-04 06:06:06 +0900

    Build : 로딩 페이지 이미지 변경

--- App.js
+++ App.js
@@ -1,19 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { View } from 'react-native';
+import Entypo from '@expo/vector-icons/Entypo';
+import * as Font from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
 import MyButton from './components/MyButton';
+import LendingPage from './components/LendingPage';
 
 const App = () => {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        await Font.loadAsync(Entypo.font);
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        await new Promise((resolve) => setTimeout(resolve, 3000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+      <LendingPage />;
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
   return (
     <View
       style={{
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: '#000',
         alignItems: 'center',
         justifyContent: 'center',
       }}
+      onLayout={onLayoutRootView}
     >
+      {/* <LendingPage /> */}
       <MyButton name={'횡단보도 상황 안내'} />
       <MyButton name={'안내 종료'} />
+      <StatusBar style="light" />
     </View>
   );
 };
