95eb9752c87ca12050a2f1bc5f02702339c485bb
MinsuKim <minsu020302@gmail.com>
2022-10-05 02:19:29 +0900

    Design : splash screen 추가

--- App.js
+++ App.js
@@ -1,46 +1,29 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View } from 'react-native';
-import Entypo from '@expo/vector-icons/Entypo';
-import * as Font from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import MyButton from './components/MyButton';
 import * as Speech from 'expo-speech';
+import Lottie from 'lottie-react-native';
 
 const App = () => {
-  const alram = "핸드폰 화면의 가운데를 기준으로 위를 누르면 안내가 시작되고 아래를 누르면 종료됩니다.";
-  const [appIsReady, setAppIsReady] = useState(false);
+  const alram =
+    '핸드폰 화면의 가운데를 기준으로 위를 누르면 안내가 시작되고 아래를 누르면 종료됩니다.';
+  const [appIsReady, setAppIsReady] = useState(true);
 
   useEffect(() => {
-    Speech.speak(alram)
-    async function prepare() {
-      try {
-        // Pre-load fonts, make any API calls you need to do here
-        await Font.loadAsync(Entypo.font);
-        // Artificially delay for two seconds to simulate a slow loading
-        // experience. Please remove this if you copy and paste the code!
-        await new Promise((resolve) => setTimeout(resolve, 3000));
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        // Tell the application to render
-        setAppIsReady(true);
-      }
-    }
-
-    prepare();
+    setTimeout(() => {
+      setAppIsReady(false);
+      Speech.speak(alram);
+    }, 4500);
   }, []);
 
-  const onLayoutRootView = useCallback(async () => {
-    if (appIsReady) {
-      await SplashScreen.hideAsync();
-    }
-  }, [appIsReady]);
-
-  if (!appIsReady) {
-    return null;
-  }
-  return (
+  return appIsReady ? (
+    <Lottie
+      source={require('./resource/animation/77592-confi-splash-screen.json')}
+      autoPlay
+      loop
+    />
+  ) : (
     <View
       style={{
         flex: 1,
@@ -48,7 +31,6 @@ const App = () => {
         alignItems: 'center',
         justifyContent: 'center',
       }}
-      onLayout={onLayoutRootView}
     >
       {/* <LendingPage /> */}
       <MyButton name={'횡단보도 상황 안내'} />
