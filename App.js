dd1cccf71faad711c1de96e8819f6e1568987578
MinsuKim <minsu020302@gmail.com>
2022-10-05 22:06:37 +0900

    Design : 디자인 변경

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import Lottie from 'lottie-react-native';
 
 const App = () => {
   const alram =
-    '핸드폰 화면의 가운데를 기준으로 위를 누르면 안내가 시작되고 아래를 누르면 종료됩니다.';
+    '화면을 클릭하면 횡단보도 상황 안내를 시작하고 다시 누르시면 종료됩니다.';
   const [appIsReady, setAppIsReady] = useState(true);
 
   useEffect(() => {
@@ -22,19 +22,21 @@ const App = () => {
       source={require('./resource/animation/77592-confi-splash-screen.json')}
       autoPlay
       loop
+      style={{
+        backgroundColor: '#5156f6',
+      }}
     />
   ) : (
     <View
       style={{
         flex: 1,
-        backgroundColor: '#000',
+        backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
       }}
     >
       {/* <LendingPage /> */}
-      <MyButton name={'횡단보도 상황 안내'} />
-      <MyButton name={'안내 종료'} />
+      <MyButton />
       <StatusBar style="light" />
     </View>
   );
