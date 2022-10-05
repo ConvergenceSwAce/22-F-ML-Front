2850773a1360df9c9566ae6d39b3dfbf9f90aaa0
MinsuKim <minsu020302@gmail.com>
2022-10-05 22:09:06 +0900

    Feat : axios 수정 및 진동피드백, TTS 추가

--- components/MyButton.js
+++ components/MyButton.js
@@ -6,61 +6,67 @@ import {
   Vibration,
   StyleSheet,
 } from 'react-native';
-import { Audio } from 'expo-av';
 import * as Speech from 'expo-speech';
+import axios from 'axios';
 
 const { width: SCREEN_WIDTH } = Dimensions.get('window');
 
-const MyButton = (props) => {
-  // 음성 출력
-  // const [sound, setSound] = useState();
-
-  // 음성을 틀어주는 비동기 함수 (백앤드에서 txt 받아오기러함)
-  // async function playSound() {
-  //   console.log('Loading Sound');
-  //   const { sound } = await Audio.Sound.createAsync(
-  //     props.name === '횡단보도 상황 안내'
-  //       ? require('../assets/sample.mp3')
-  //       : require('../assets/end.mp3')
-  //   );
-  //   setSound(sound);
-
-  //   console.log('test Sound');
-  //   await sound.playAsync();
-  // }
-
-  // useEffect(() => {
-  //   return sound
-  //     ? () => {
-  //         // 메모리 낭비 줄이기
-  //         console.log('Unloading Sound');
-  //         sound.unloadAsync();
-  //       }
-  //     : undefined;
-  // }, [sound]);
-
-  // 헵틱 반응 시간
+const MyButton = () => {
   const HUNDRED_MS = 250;
   const PATTERN = [1 * HUNDRED_MS, 2 * HUNDRED_MS];
+  const btnName = ['횡단보도 상황 안내', '안내 종료'];
+  const [num, setNum] = useState(0);
+  const [text, setText] = useState('');
+  const [flag, setFlag] = useState(false);
+  let loop;
 
+  useEffect(() => {
+    if (flag) {
+      clearInterval(loop);
+    } else {
+      Speech.speak(text);
+    }
+  }, [text]);
+
+  const Resdata = () => {
+    axios
+      .get('http://192.168.235.76:5001/api/notification')
+      .then((res) => {
+        setText(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const start = () => {
+    console.log(flag + 'start');
+    setNum(1);
+    setFlag(false);
+    Speech.stop();
+    Speech.speak('안내를 시작합니다');
+    loop = setInterval(() => Resdata(), 2000);
+  };
+
+  const stop = () => {
+    console.log(flag + 'stop');
+    setNum(0);
+    setFlag(true);
+    Speech.stop();
+    Speech.speak('안내를 종료합니다');
+    clearInterval(loop);
+  };
 
   return (
     <TouchableOpacity
       style={styles.button}
       onPress={() => {
-        if (props.name === '횡단보도 상황 안내') {
-          // playSound();
-          Speech.stop();
-          Speech.speak("test")
-        } else {
-          Speech.stop();
-          Speech.speak("안내를 종료합니다")
-          // playSound();
-        }
+        Speech.stop();
         Vibration.vibrate(PATTERN[0]);
+        num === 0 ? start() : stop();
       }}
     >
-      <Text style={styles.buttonName}>{props.name}</Text>
+      <Text style={styles.buttonName}>{btnName[num]}</Text>
     </TouchableOpacity>
   );
 };
@@ -69,7 +75,7 @@ export default MyButton;
 
 const styles = StyleSheet.create({
   button: {
-    backgroundColor: '#071D49',
+    backgroundColor: '#5156f6',
     width: SCREEN_WIDTH,
     flex: 1,
     marginTop: '0.5%',
